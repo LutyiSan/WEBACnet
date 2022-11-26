492f0f92ab791952fc1fb7f04f82dc293e93d025
Александр Луценко <avl@nppelement.com>
2022-11-26 10:55:29 +0300

    update web

--- tools/route_func.py
+++ tools/route_func.py
@@ -1,5 +1,6 @@
 from flask import request
 from operation import bacnet_whois, bacnet_obj_list, read_properties
+import random
 
 # i_am_dict = {'device-ip': ['21', '22', '23'], 'device-id': [21, 22, 23], 'device_name': ['n1', 'n2', 'n3'],
 #           'vendor': ['u', 'u', 'u']}
@@ -18,25 +19,27 @@ def who_is():
     if isinstance(result, dict):
         return make_wi_response(result)
     else:
-        return '<ol><li class="device">Not answer from devices</li></ol>'
+        return '<p>Not answer from devices</p>'
 
 
 def make_wi_response(devices):
+
     resp = '<div class="for-ol">'
     len_devices = len(devices['device-ip'])
     i = -1
     while i < (len_devices - 1):
+        col = random.choice(['#D94C4C', '#D9904C', '#D6D94C', '#4FD94C', '#4CD9D1', '#4C6BD9', '#A34CD9'])
         i += 1
-        resp += (f'<p class="device">ID: {devices["device-id"][i]} IP: {devices["device-ip"][i]} '
-                 f'NAME: {devices["device_name"][i]} VENDOR: {devices["vendor"][i]}</p>')
+        image = 'static/icons/device-icon.svg'
+        resp += f"<p class='device'><img src='{image}'>  ID: {devices['device-id'][i]} IP: {devices['device-ip'][i]} NAME: {devices['device_name'][i]} VENDOR: {devices['vendor'][i]}</p>"
     resp += '</div>'
     return resp
 
 
 def get_object_list():
     params = request.args.get("device").split(' ')
-    curr_device['ip'] = params[3]
-    curr_device['id'] = int(params[1])
+    curr_device['ip'] = params[4]
+    curr_device['id'] = int(params[2])
     #   port = int(request.args.get("port"))
     #   host_ip = request.args.get("host-ip")
     result = bacnet_obj_list(bac_socket['ip'], bac_socket['port'], curr_device['ip'], curr_device['id'])
@@ -51,8 +54,18 @@ def make_obj_list_response(obj_list):
     len_objects = len(obj_list['type'])
     i = -1
     while i < (len_objects - 1):
+        col = random.choice(['#D94C4C', '#D9904C', '#D6D94C', '#4FD94C', '#4CD9D1', '#4C6BD9', '#A34CD9'])
         i += 1
-        resp += f'<p class="object">{obj_list["type"][i]} {obj_list["id"][i]}</p>'
+        image = ''
+        if "Input" in obj_list["type"][i]:
+            image = 'static/icons/in_obj.svg'
+
+        elif "Output" in obj_list["type"][i]:
+            image = 'static/icons/out_obj.svg'
+
+        else:
+            image = 'static/icons/value_obj.svg'
+        resp += f'<p class="object"><img src="{image}">  {obj_list["type"][i]} {obj_list["id"][i]}</p>'
     resp += '</div>'
     return resp
 
@@ -60,13 +73,14 @@ def make_obj_list_response(obj_list):
 def get_object_props():
     print('IN func prop')
     params = request.args.get("object").split(' ')
-    curr_object['type'] = params[0]
-    curr_object['id'] = params[1]
+    curr_object['type'] = params[1]
+    curr_object['id'] = params[2]
     result = read_properties(bac_socket['ip'], bac_socket['port'], curr_device['ip'], curr_object['type'], curr_object['id'])
     if result:
         response = ''
-        res = str(result).split(',')
+      #  res = str(result).split(',')
         for i in result:
+          #  col = random.choice(['#D94C4C', '#D9904C', '#D6D94C', '#4FD94C', '#4CD9D1', '#4C6BD9', '#A34CD9'])
             response += f"<p>{i}</p>"
         return response
     else:
